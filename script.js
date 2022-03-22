0f098dcf6c94d64c67dbbc56a1b61d6122a45c39
sungwookang <sungwookang11@gmail.com>
2022-03-22 15:41:08 +0900

    flying leaves

---
+++ script.js
@@ -0,0 +1,16 @@
+const actionFl = document.querySelector(".flowerCon");
+const topL = document.querySelector(".topL");
+const leftL = document.querySelector(".leftL");
+const rightL = document.querySelector(".rightL");
+const bottomL = document.querySelector(".bottomL");
+
+
+
+const handleClick = () => {
+    topL.classList.toggle("active");
+    leftL.classList.toggle("active");
+    rightL.classList.toggle("active");
+    bottomL.classList.toggle("active");
+}
+
+actionFl.addEventListener("click", handleClick);
